44a2827ecb170e7e325388e432978c9f3aa6a990
kaiser-lai <klai2027@kehillahstudent.org>
2025-02-10 09:17:35 -0800

    done

--- my_bot.py
+++ my_bot.py
@@ -1,74 +1,7 @@
 import random
-"""
-**Do NOT change the name of this function.**
-
-This function will be called every time anyone says anything on a channel where the bot lives.
-
-* It returns `True` if the bot notices something it wants to repond to.
-* You can have certain words or patterns in the messages trigger the bot.
-* You can have the bot respond differently to different users
-"""
+state = "not russian"
 def should_i_respond(user_message, user_name):
-  return True
-
-async def russian(ctx, mode: str = None):
-  if mode not in ["casual", "deathwish"]:
-    await ctx.send ("select your game mode... deathwish or casual")
-    return
-  if mode == "casual":
-    await ctx.send("1 bullet loaded, enter too fire and type c to pass to bot")
-    cylinder = ["click"]*5 + ["boom"]
-    
-  elif mode == "deathwish":
-      await ctx.send("2 bullet loaded, enter too fire and type c to pass to bot")
-      cylinder = ["click"]*4 + 2["boom"]
-    
-  random.shuffle
-      
-  def check(m):o
-        return m.user == ctx.user and ctx.content.lower() in ["fire", "c"]
-      
-  while cylinder:
-        msg = await ctx.bot.wait_for("message", check=check)
-        if msg.content.lower() == "fire":
-          trigger = cylinder.pop()
-          await ctx.send("{trigger.upper}!")
-          if trigger == "boom":
-            await ctx.send("lol u ded")
-            return
-          
-          elif msg.content.lower() == "c":
-            await ctx.send ("bot's turn ...")
-            if trigger == "boom":
-              await ctx.send("bot ded u win")
-            return
-          if not cylinder:
-                  await ctx.send("🔄 Reloading...")
-                  cylinder = ["click"] * 5 + ["boom"]
-                  random.shuffle(cylinder)
-
-
-        
-
-      
-
-
-
-
-  
-
-    
-        
-    
-        
-
-
-
-
-        
-        
-
-  
+    return True
 
 
 
@@ -82,17 +15,47 @@ async def russian(ctx, mode: str = None):
 #* You can have the bot respond differently to different messages and users
 
 def respond(user_message, user_name):
-  if "do you speak mandarin":
+  global state, cylinder
+  if "do you speak mandarin" in user_message:
     return f""" no... ingles por favor
   {user_message.replace("robot", user_name)}"""
   elif "cats" in user_message:
     return "I eat them"
   if "russian"in user_message:
-    return russian()
-  if "i like dogs"in user_message:
-    return "i eat them"
-  if "potato"in user_message:
-    return "potato"
+    state = "start"
+    return "Let's play, shall we... casual or deathwish? "
+  
+  
+  if "russian" in user_message:
+        state = "start"
+        return "Let's play, shall we... casual or deathwish? "
+
+  if state == "start":
+        if user_message == "deathwish":
+            state = "deathwish"
+            cylinder = ["click"] * 4 + ["boom"] * 2
+            random.shuffle(cylinder)
+            return "2 bullets loaded, press e to start"
+
+  if state == "deathwish":
+        if user_message == "e":
+            if not cylinder:
+                cylinder = ["click"] * 4 + ["boom"] * 2
+                random.shuffle(cylinder)
+                return "Reloading... press e to continue"
+
+            trigger = cylinder.pop()
+            if trigger == "boom":
+                state = "not russian"
+                return "game over"
+            else:
+                return "Click! You're still alive. Press enter to pull the trigger again."
+            
+        if "i like dogs" in user_message:
+          return "I eat them"
+
+        if "potato" in user_message:
+          return "potato"
   if "who is your daddy"in user_message:
     return "my creator is kaiser my father"
   if "favorite color"in user_message:
@@ -101,7 +64,7 @@ def respond(user_message, user_name):
     return "who is that.. idk"
   if "english or spanish"in user_message:
     return "no hablo inglese y espanol"
-  if"what is your favorite music?"in user_message:
+  if "what is your favorite music?"in user_message:
     return "listen to break it out by rock summer"
 
 
